627793d1a7329c6f14e8f91b88b48d1f992841c6
RusticBard <sanket.p.kumbhar.2805@gmail.com>
2026-02-22 23:25:29 +0530

    added option to save the last theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,4 +3,26 @@ require("core.keymaps")
 require("core.autocmd")
 require("config.lazy")
 
-vim.cmd([[colorscheme carbonfox]])
+local theme_cache = vim.fn.stdpath("data") .. "/last_theme.txt"
+
+do
+	local theme_file = io.open(theme_cache, "r")
+	if theme_file then
+		local saved_theme = theme_file:read("*all"):gsub("%s+", "")
+		theme_file:close()
+		pcall(vim.cmd.colorscheme, saved_theme)
+	end
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	group = vim.api.nvim_create_augroup("SaveTheme", { clear = true }),
+	callback = function(args)
+		if args.match ~= "" then
+			local theme_file = io.open(theme_cache, "w")
+			if theme_file then
+				theme_file:write(args.match)
+				theme_file:close()
+			end
+		end
+	end,
+})
