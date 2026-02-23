05a89624e271046f0bad0ef5dd386ef803d8cc33
RusticBard <sanket.p.kumbhar.2805@gmail.com>
2026-02-23 11:05:59 +0530

    added colorschemes to neovim

---
+++ nvim/.config/nvim/lua/themes/dracula.lua
@@ -0,0 +1,32 @@
+return {
+	-- add dracula
+	"Mofiqul/dracula.nvim",
+
+	-- Configure LazyVim to load dracula
+	opts = {
+		colorscheme = "dracula",
+
+		-- show the '~' characters after the end of buffers
+		show_end_of_buffer = true, -- default false
+		-- use transparent background
+		-- transparent_bg = true, -- default false
+		-- set custom lualine background color
+		-- lualine_bg_color = "#44475a", -- default nil
+		-- set italic comment
+		italic_comment = true, -- default false
+		-- overrides the default highlights with table see `:h synIDattr`
+		overrides = {},
+		-- You can use overrides as table like this
+		-- overrides = {
+		--   NonText = { fg = "white" }, -- set NonText fg to white
+		--   NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
+		--   Nothing = {} -- clear highlight of Nothing
+		-- },
+		-- Or you can also use it like a function to get color from theme
+		-- overrides = function (colors)
+		--   return {
+		--     NonText = { fg = colors.white }, -- set NonText fg to white of theme
+		--   }
+		-- end,
+	},
+}
